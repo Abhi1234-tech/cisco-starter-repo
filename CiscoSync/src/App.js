e1a7bfa2f4278ee985c6292a9819c4d0c8fd2051
Abhishek Girnar <64825778+Abhi1234-tech@users.noreply.github.com>
2023-08-02 08:41:18 +0000

    v4

--- CiscoSync/src/App.js
+++ CiscoSync/src/App.js
@@ -1,16 +1,37 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./header.js";
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [ipAddress, setIpAddress] = useState("");
+
+  useEffect(() => {
+    fetch("https://api64.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+       
+        const ipAddress = data.ip;
+        setIpAddress(ipAddress);
+      })
+      .catch((error) => {
+        console.error("Error fetching IP:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
+      <Header />
+      <div className="Display">
+      Your IP is: {ipAddress}
+      </div>
+      <div class="video-background">
+       <video src="https://cdn.pixabay.com/vimeo/334689174/transition-23325.mp4?width=1280&hash=d8343efddf13348bea9f287f6ff1f749a14e53d4" autoPlay muted>
       
-      <Header/>
+      
+    </video>
+  </div>
     </div>
   );
 }
 
 export default App;
-
